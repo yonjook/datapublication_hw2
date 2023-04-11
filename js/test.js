3c2d96e43fa2007de3992663d6e3af10158e745b
yonjook <yonjook@uchicago.edu>
2023-04-10 22:14:56 -0500

    hw3

---
+++ js/test.js
@@ -0,0 +1,17 @@
+function hideElement() {
+    const image = document.getElementById('hello');
+    image.style.display = 'none'
+}
+
+
+
+function displayMinute() {
+    const minuteDiv = document.getElementById('minute');
+    const date = new Date();
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const totalMinutes = hours * 60 + minutes;
+    minuteDiv.innerText = `It has been ${totalMinutes} minutes since midnight.`;
+  }
+  
+
